5c5fd4b22237d0d5ff0d863ca39b2eb4737bb66d
Eduardo Veiga <epontesveiga@gmail.com>
2023-09-14 16:23:06 -0300

    architecture

--- ula.vhd
+++ ula.vhd
@@ -5,3 +5,10 @@ entity mux2x1 is
         saida           :   out std_logic
     );
 end entity;
+
+architecture a_mux2x1 of mux2x1 is
+begin
+    saida   <=  entr0   when    sel='0' else
+                entr1   when    sel='1'
+                '0';
+end architecture;
